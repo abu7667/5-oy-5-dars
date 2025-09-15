5eb53414947b0d70d1bda8ae7af8fa6ba1211175
abu7667 <fijiim3@gmail.com>
2025-09-15 15:37:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let ism = prompt("Ismingizni kiriting:");
+let id = prompt("IDingizni kiriting:");
+let password = prompt("Passwordingizni kiritng")
+
+let obj = {
+    name: ism,
+    Id: id,
+    password: password
+}
+console.log(obj);
